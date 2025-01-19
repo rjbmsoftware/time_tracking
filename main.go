551972a7f540337142197116890ba744c2fc5566
richard.millett <richard.millett+github@googlemail.com>
2025-01-19 15:55:47 +0000

    function POST project that uses the database

--- main.go
+++ main.go
@@ -16,8 +16,8 @@ func main() {
 	}
 
 	db.AutoMigrate(&Project{})
-	db.Create(&Project{ID: 1, Name: "test"})
-	db.Create(&Project{ID: 2, Name: "another test"})
+	db.Create(&Project{Name: "test"})
+	db.Create(&Project{Name: "another test"})
 
 	router := gin.Default()
 	router.Use(func(c *gin.Context) {
@@ -26,13 +26,13 @@ func main() {
 	})
 	router.GET("/projects", getProjects)
 	router.GET("/projects/:id", getProject)
-	// router.POST("/projects", postProjects)
+	router.POST("/projects", postProjects)
 
 	router.Run("localhost:8080")
 }
 
 type Project struct {
-	ID   int    `json:"id"`
+	gorm.Model
 	Name string `json:"name"`
 }
 
@@ -57,7 +57,7 @@ func getProject(c *gin.Context) {
 	db := c.MustGet("db").(*gorm.DB)
 
 	var project Project
-	if err := db.First(&project, "ID = ?", id).Error; err == nil {
+	if err := db.First(&project, "id = ?", id).Error; err == nil {
 		c.IndentedJSON(http.StatusOK, project)
 		return
 	}
@@ -65,13 +65,20 @@ func getProject(c *gin.Context) {
 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "project not found"})
 }
 
-// func postProjects(c *gin.Context) {
-// 	var newProject Project
+func postProjects(c *gin.Context) {
+	var newProject Project
 
-// 	if err := c.BindJSON(&newProject); err != nil {
-// 		return
-// 	}
+	if err := c.BindJSON(&newProject); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "error"})
+		return
+	}
 
-// 	projects = append(projects, newProject)
-// 	c.IndentedJSON(http.StatusCreated, newProject)
-// }
+	db := c.MustGet("db").(*gorm.DB)
+	result := db.Create(&newProject)
+	if result.Error != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": result.Error})
+		return
+	}
+
+	c.IndentedJSON(http.StatusCreated, newProject)
+}
